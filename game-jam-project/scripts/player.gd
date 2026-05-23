a71882386e77388b4cefb453e886e93793cfeac6
Minipl0p <paul.chazalmartin@protonmail.com>
2026-05-24 01:52:20 +0200

    pause animation player

--- game-jam-project/scripts/player.gd
+++ game-jam-project/scripts/player.gd
@@ -9,24 +9,26 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 func _ready() -> void:
-	sprite.play("walk")
+	pass
 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += grav * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		sprite.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		sprite.pause()
+	
+	if direction.x != 0:
+		sprite.flip_h = direction.x < 0
 	
 	move_and_slide()
 
