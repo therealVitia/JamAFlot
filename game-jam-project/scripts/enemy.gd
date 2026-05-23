f25d0452c0c6da39033f6c81853199878ce78194
Minipl0p <paul.chazalmartin@protonmail.com>
2026-05-23 12:07:31 +0200

    enemy navigation problem

---
+++ game-jam-project/scripts/enemy.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var player: CharacterBody3D
+@export var speed: float = 3.5
+
+@onready var navigation_agent = $NavigationAgent3D
+@onready var sprite = $Sprite3D
+@onready var animation_player = $AnimationPlayer
+
+const GRAVITY = -9.8
+
+func _ready() -> void:
+	navigation_agent.target_position = player.global_position
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	navigation_agent.target_position = player.global_position
+	var direction = navigation_agent.get_next_path_position() - global_position
+	direction.y = 0.0
+	direction = direction.normalized()
+
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	if direction.x != 0:
+		sprite.flip_h = direction.x < 0
+
+	move_and_slide()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D and body.name == "Player":
+		print("ennemi touche le joueur !")
