f6d98f20b377ad840a75ccbd65717f346eb1eac0
Vitia <vmurat@student.42angouleme.fr>
2026-05-24 05:02:23 +0200

    fixed more merge conflicts

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,16 @@
 class_name Player
 extends CharacterBody3D
 
-<<<<<<< HEAD:scripts/player.gd
-=======
 signal hit
 
 @onready var sprite: AnimatedSprite3D = $AnimatedSprite3D
->>>>>>> player:game-jam-project/scripts/player.gd
 const grav = -9.81
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-<<<<<<< HEAD:scripts/player.gd
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += grav * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-=======
 func _ready() -> void:
 	sprite.play("walk")
 
-
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += grav * delta
@@ -36,7 +18,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
->>>>>>> player:game-jam-project/scripts/player.gd
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -47,15 +28,3 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-<<<<<<< HEAD:scripts/player.gd
-=======
-
-func _on_detection_zone_body_entered(body: Node3D) -> void:
-	if body.name == "Enemy":
-		hit.emit()
-		print("touché !")
-
-
-func start(pos: Vector3) -> void:
-	position = pos
->>>>>>> player:game-jam-project/scripts/player.gd
